76ada0cafda3087e4288a38f99fbf5d03884c16b
kunalmenda <kunal.menda@gmail.com>
2017-03-19 00:59:38 -0700

    EVEN Newer ran and tested
    Agents observe Fire Health

--- FirestormProject/FireExtinguishing/Logs/plotting.m
+++ FirestormProject/FireExtinguishing/Logs/plotting.m
@@ -41,3 +41,47 @@ legend 'Learned MLP+GRU Policy' 'Largest Need Policy' 'Closest Fire Policy' 'Loc
 title '20 Fires, 10 UAVs'
 
 print -dpng -r300 'TrainingCurve_Newest_10U20F.png'
+
+avg_mystrategy = 10.9093980249;
+std_mystrategy = 2.08991266819;
+
+avg_naive = 10.4368430991; 
+std_naive = 1.74412980459;
+
+%% EVEN Newer Env 10F20U
+
+
+load ./EVEN_Newer_10U20F_GRU/progress.mat
+
+avg_mystrategy = 10.9093980249;
+std_mystrategy = 2.08991266819;
+
+avg_naive = 10.4368430991; 
+std_naive = 1.74412980459;
+
+hc = figure();
+set(hc,'PaperUnits','Points');
+set(hc,'PaperPosition',[650,550,350,300]);
+set(hc,'Units','Points');
+set(hc,'Position',[650,550,350,300]);
+
+plot(Iteration, AverageDiscountedReturn);
+hold on
+C = [0.2123    0.2138    0.6270];
+plot([0,max(Iteration)],[avg_mystrategy, avg_mystrategy]);
+plot([0,max(Iteration)],[avg_naive, avg_naive]);
+plot([0,max(Iteration)],[avg_mystrategy, avg_mystrategy]+std_mystrategy,'--','Color',C )
+plot([0,max(Iteration)],[avg_mystrategy, avg_mystrategy]-std_mystrategy,'--','Color',C )
+
+plot([0,max(Iteration)],[avg_naive, avg_naive]+std_naive,'--','Color',C )
+plot([0,max(Iteration)],[avg_naive, avg_naive]-std_naive,'--','Color',C )
+grid on
+
+set(gca,'xlim',[0,500]);
+
+xlabel('Iteration')
+ylabel('Avg Discounted Return')
+legend 'Learned MLP+GRU Policy' 'Largest Need Policy' 'Closest Fire Policy' 'Location' 'Best'
+title '20 Fires, 10 UAVs'
+
+print -dpng -r300 'TrainingCurve_EVEN_Newer_10U20F.png'
